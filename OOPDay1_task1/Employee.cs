37d2455b2572198440479a237d6b771c9e1b8306
Khaled Helmy Mahran <khaledhelmy774@gmail.com>
2024-11-20 00:53:46 +0300

    Add project files.

---
+++ OOPDay1_task1/Employee.cs
@@ -0,0 +1,78 @@
+﻿namespace OOPDay1_task1
+{
+    internal class Employee
+    {
+
+      public  int id;
+      public  string name;
+      public  string address;
+      private string phone;
+      private int salary;
+
+      public void setSalary(int s)
+        {
+            if (s < 6000)
+            {
+                salary = 6000;
+            }
+            else
+            {
+                salary = s;
+            }
+        }
+
+
+        public bool phoneNumber(string number)
+        {
+            if (number[2] == '0')
+            {
+                return number[0] == '0' && number[1] == '1' & number.Length == 12 && isDigit(number);
+            }
+
+            else if (number[2] == '1')
+            {
+                return number[0] == '0' && number[1] == '1' & number.Length == 12 && isDigit(number);
+            }
+
+
+            else if (number[2] == '2')
+            {
+                return number[0] == '0' && number[1] == '1' & number.Length == 12 && isDigit(number);
+            }
+
+            else if (number[2] == '5')
+            {
+                return number[0] == '0' && number[1] == '1' & number.Length == 12 && isDigit(number);
+            }
+
+            else {
+                return false;
+            }
+
+        }
+       public bool isDigit(string input)
+        {
+
+            foreach (char c in input)
+            {
+                if (c < '0' || c > '9')
+                    return false;
+            }
+
+            return true;
+
+        }
+
+        public void IncreaseSalary(int increase)
+        {
+            salary += increase;
+            Console.WriteLine(salary);
+
+        }
+        public void Display()
+        {
+            Console.WriteLine($"name is {name} \n id is {id} \n address is {address} \n phone is {phone}\n salary is {salary} ");
+        }
+        
+    }
+}
---
+++ OOPDay1_task1/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace OOPDay1_task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.id = 1;
+            employee.name = "khaled";
+            employee.setSalary(7000);
+            employee.phoneNumber("01128109017");
+            employee.IncreaseSalary(300);
+
+
+            Employee employee2 = new Employee();
+            employee2.id = 2;
+            employee2.name = "ali";
+            employee2.setSalary(20000);
+            employee2.phoneNumber("01528109017");
+
+
+            Employee employee3 = new Employee();
+            employee3.id = 3;
+            employee3.name = "mohamed";
+            employee3.setSalary(5000);
+            employee3.phoneNumber("01028109017");
+            employee3.IncreaseSalary(1000);
+
+
+            Employee[] allEmployee = new Employee[3];
+            allEmployee[0] = employee;
+            allEmployee[1] = employee2;
+            allEmployee[2] = employee3;
+
+
+            for (int i = 0; i < allEmployee.Length; i++)
+            {
+                allEmployee[i].Display();
+            }
+
+
+
+        }
+    }
+}
